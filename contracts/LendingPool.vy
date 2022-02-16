25aead2577498b2e7e8ab37918269a5ca8479373
Diogo Pires <diogo@zharta.com>
2022-02-16 17:46:14 +0000

    POC-179 added distribution of rewards to lenders

--- contracts/LendingPool.vy
+++ contracts/LendingPool.vy
@@ -1,10 +1,11 @@
-# @version ^0.3.0
+# @version ^0.3.1
 
 
 import interfaces.ILendingPool as LendingPoolInterface
 
 interface ERC20Token:
   def allowance(_owner: address, _spender: address) -> uint256: view
+  def decimals() -> uint256: view
   def transfer(_recipient: address, _amount: uint256) -> bool: nonpayable
   def transferFrom(_sender: address, _recipient: address, _amount: uint256): nonpayable
 
@@ -13,17 +14,14 @@ implements: LendingPoolInterface
 
 
 struct InvestorFunds:
-  currentAmountInvested: uint256
-  totalAmountInvested: uint256
+  currentAmountDeposited: uint256
+  totalAmountDeposited: uint256
   totalAmountWithdrawn: uint256
+  currentPendingRewards: uint256
+  totalRewardsAmount: uint256
+  activeForRewards: bool
 
-struct InvestorFundsOutput:
-  currentAmountInvested: uint256
-  rewards: uint256
-  totalAmountInvested: uint256
-  totalAmountWithdrawn: uint256
-
-event Investment:
+event Deposit:
   _from: address
   amount: uint256
   erc20TokenContract: address
@@ -31,6 +29,10 @@ event Investment:
 event Withdrawal:
   _from: address
   amount: uint256
+  erc20TokenContract: address
+
+event Compound:
+  _from: address
   rewards: uint256
   erc20TokenContract: address
 
@@ -49,13 +51,13 @@ owner: public(address)
 loansContract: public(address)
 erc20TokenContract: public(address)
 
-funds: HashMap[address, InvestorFunds]
+funds: public(HashMap[address, InvestorFunds])
+depositors: public(DynArray[address, 2**50])
 
 fundsAvailable: public(uint256)
 fundsInvested: public(uint256)
 totalFundsInvested: public(uint256)
 
-rewards: public(uint256)
 totalRewards: public(uint256)
 
 
@@ -73,56 +75,34 @@ def _fundsAreAllowed(_owner: address, _spender: address, _amount: uint256) -> bo
   return _amount <= amountAllowed
 
 
-@view
-@internal
-def _userRewards(_userAddr: address) -> uint256:
-  if self.rewards == 0:
-    return 0
-  elif self.totalFundsInvested == 0:
-    return 0
-  elif self.funds[_userAddr].currentAmountInvested == 0:
-    return 0
-  else:
-    return self.rewards / (self.funds[_userAddr].currentAmountInvested / (self.fundsAvailable + self.fundsInvested))
-
-
-@view
-@external
-def userFunds(_userAddr: address) -> InvestorFundsOutput:
-  return InvestorFundsOutput(
-    {
-      currentAmountInvested: self.funds[_userAddr].currentAmountInvested,
-      rewards: self._userRewards(_userAddr),
-      totalAmountInvested: self.funds[_userAddr].totalAmountInvested,
-      totalAmountWithdrawn: self.funds[_userAddr].totalAmountWithdrawn
-    }
-  )
-
-
 # _amount should be passed in wei
 @payable
 @external
 def deposit(_amount: uint256) -> InvestorFunds:
-  assert _amount > 0, "Amount invested has to be higher than 0"
+  assert _amount > 0, "Amount deposited has to be higher than 0"
   assert self._fundsAreAllowed(msg.sender, self, _amount), "Insufficient funds allowed to be transfered"
 
   ERC20Token(self.erc20TokenContract).transferFrom(msg.sender, self, _amount)
 
-  if self.funds[msg.sender].totalAmountInvested > 0:
-    self.funds[msg.sender].totalAmountInvested += _amount
-    self.funds[msg.sender].currentAmountInvested += _amount
+  if self.funds[msg.sender].totalAmountDeposited > 0:
+    self.funds[msg.sender].totalAmountDeposited += _amount
+    self.funds[msg.sender].currentAmountDeposited += _amount
   else:
     self.funds[msg.sender] = InvestorFunds(
       {
-        currentAmountInvested: _amount,
-        totalAmountInvested: _amount,
-        totalAmountWithdrawn: 0
+        currentAmountDeposited: _amount,
+        totalAmountDeposited: _amount,
+        totalAmountWithdrawn: 0,
+        currentPendingRewards: 0,
+        totalRewardsAmount: 0,
+        activeForRewards: True
       }
     )
+    self.depositors.append(msg.sender)
 
   self.fundsAvailable += _amount
 
-  log Investment(msg.sender, _amount, self.erc20TokenContract)
+  log Deposit(msg.sender, _amount, self.erc20TokenContract)
 
   return self.funds[msg.sender]
 
@@ -130,21 +110,41 @@ def deposit(_amount: uint256) -> InvestorFunds:
 # _amount should be passed in wei
 @external
 def withdraw(_amount: uint256) -> InvestorFunds:
-  assert self.funds[msg.sender].currentAmountInvested > 0, "The sender has no funds invested"
-  assert self.funds[msg.sender].currentAmountInvested >= _amount, "The sender has less funds invested than the amount requested"
+  assert self.funds[msg.sender].currentAmountDeposited > 0, "The sender has no funds deposited"
+  assert self.funds[msg.sender].currentAmountDeposited >= _amount, "The sender has less funds deposited than the amount requested"
   assert self.fundsAvailable >= _amount, "Not enough funds in the pool to be withdrawn"
 
   ERC20Token(self.erc20TokenContract).transfer(msg.sender, _amount)
 
-  rewardsFromUser: uint256 = self.rewards / (self.funds[msg.sender].currentAmountInvested / (self.fundsAvailable + self.fundsInvested))
-  self.rewards -= rewardsFromUser
-
-  self.funds[msg.sender].currentAmountInvested -= _amount
+  self.funds[msg.sender].currentAmountDeposited -= _amount
   self.funds[msg.sender].totalAmountWithdrawn += _amount
+  
+  if self.funds[msg.sender].currentAmountDeposited == 0:
+    ERC20Token(self.erc20TokenContract).transfer(msg.sender, self.funds[msg.sender].currentPendingRewards)
+    self.funds[msg.sender].currentPendingRewards = 0
+    self.funds[msg.sender].activeForRewards = False
   
   self.fundsAvailable -= _amount
 
-  log Withdrawal(msg.sender, _amount, rewardsFromUser, self.erc20TokenContract)
+  log Withdrawal(msg.sender, _amount, self.erc20TokenContract)
+
+  return self.funds[msg.sender]
+
+
+@external
+def compoundRewards() -> InvestorFunds:
+  assert self.funds[msg.sender].currentAmountDeposited > 0, "The sender has no funds deposited"
+  assert self.funds[msg.sender].currentPendingRewards > 0, "The sender has no pending rewards to compound"
+
+  pendingRewards: uint256 = self.funds[msg.sender].currentPendingRewards
+
+  self.funds[msg.sender].currentAmountDeposited += pendingRewards
+  self.funds[msg.sender].totalAmountDeposited += pendingRewards
+  self.funds[msg.sender].currentPendingRewards = 0
+
+  self.fundsAvailable += pendingRewards
+
+  log Compound(msg.sender, pendingRewards, self.erc20TokenContract)
 
   return self.funds[msg.sender]
 
@@ -153,7 +153,7 @@ def withdraw(_amount: uint256) -> InvestorFunds:
 def sendFunds(_to: address, _amount: uint256) -> uint256:
   assert msg.sender == self.loansContract, "The sender's address is not the loans contract address"
   assert _amount > 0, "The amount to send should be higher than 0"
-  assert _amount <= self.fundsAvailable, "No sufficient invested funds to perform the transaction"
+  assert _amount <= self.fundsAvailable, "No sufficient deposited funds to perform the transaction"
 
   ERC20Token(self.erc20TokenContract).transfer(_to, _amount)
 
@@ -166,21 +166,28 @@ def sendFunds(_to: address, _amount: uint256) -> uint256:
   return self.fundsAvailable
 
 
-@payable
 @external
 def receiveFunds(_owner: address, _amount: uint256, _rewardsAmount: uint256) -> uint256:
   assert msg.sender == self.loansContract, "The sender's address is not the loans contract address"
   assert self._fundsAreAllowed(_owner, self, _amount + _rewardsAmount), "Insufficient funds allowed to be transfered"
   assert _amount + _rewardsAmount > 0, "The sent value should be higher than 0"
-  assert _amount <= self.fundsInvested, "There are more funds being sent than expected by the invested funds variable"
+  assert _amount <= self.fundsInvested, "There are more funds being sent than expected by the deposited funds variable"
 
   ERC20Token(self.erc20TokenContract).transferFrom(_owner, self, _amount + _rewardsAmount)
-
-  self.fundsAvailable += _amount + _rewardsAmount
-  self.fundsInvested -= _amount
   
   self.totalRewards += _rewardsAmount
-  self.rewards += _rewardsAmount
+
+  totalTvl: uint256 = self.fundsAvailable + self.fundsInvested
+  rewardsFromUser: uint256 = 0
+
+  for depositor in self.depositors:
+    if self.funds[depositor].activeForRewards:
+      rewardsFromUser = _rewardsAmount * self.funds[depositor].currentAmountDeposited / totalTvl
+      self.funds[depositor].currentPendingRewards += rewardsFromUser
+      self.funds[depositor].totalRewardsAmount += rewardsFromUser
+
+  self.fundsAvailable += _amount
+  self.fundsInvested -= _amount
 
   log FundsReceipt(msg.sender, _amount, _rewardsAmount, self.erc20TokenContract)
 
@@ -190,4 +197,5 @@ def receiveFunds(_owner: address, _amount: uint256, _rewardsAmount: uint256) -> 
 @external
 @payable
 def __default__():
-  raise "No function called!"
+  if msg.value > 0:
+    send(msg.sender, msg.value)
--- tests/test_lendingpool.py
+++ tests/test_lendingpool.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 import pytest
 import brownie
 
@@ -40,8 +41,8 @@ def test_initial_state(lending_pool_contract, erc20_contract, contract_owner):
     assert lending_pool_contract.erc20TokenContract() == erc20_contract
 
 
-def test_invest_zero_investment(lending_pool_contract, investor):
-    with brownie.reverts("Amount invested has to be higher than 0"):
+def test_deposit_zero_investment(lending_pool_contract, investor):
+    with brownie.reverts("Amount deposited has to be higher than 0"):
         lending_pool_contract.deposit(0, {"from": investor})
 
 
@@ -62,48 +63,51 @@ def test_deposit(lending_pool_contract, erc20_contract, investor, contract_owner
     erc20_contract.mint(investor, Web3.toWei(1, "ether"), {"from": contract_owner})
     erc20_contract.approve(lending_pool_contract, Web3.toWei(1, "ether"), {"from": investor})
     
-    tx_invest = lending_pool_contract.deposit(Web3.toWei(1, "ether"), {"from": investor})
+    tx_deposit = lending_pool_contract.deposit(Web3.toWei(1, "ether"), {"from": investor})
 
-    funds_from_address = tx_invest.return_value
+    funds_from_address = tx_deposit.return_value
 
-    assert funds_from_address["currentAmountInvested"] == Web3.toWei(1, "ether")
-    assert funds_from_address["totalAmountInvested"] == Web3.toWei(1, "ether")
+    assert funds_from_address["currentAmountDeposited"] == Web3.toWei(1, "ether")
+    assert funds_from_address["totalAmountDeposited"] == Web3.toWei(1, "ether")
     assert funds_from_address["totalAmountWithdrawn"] == 0
+    assert funds_from_address["currentPendingRewards"] == 0
+    assert funds_from_address["totalRewardsAmount"] == 0
+    assert funds_from_address["activeForRewards"] == True
     assert lending_pool_contract.fundsAvailable() == Web3.toWei(1, "ether")
 
-    assert len(tx_invest.events) == 3
-    assert tx_invest.events[-1]["_from"] == investor
-    assert tx_invest.events[-1]["amount"] == Web3.toWei(1, "ether")
-    assert tx_invest.events[-1]["erc20TokenContract"] == erc20_contract
+    assert len(tx_deposit.events) == 3
+    assert tx_deposit.events[-1]["_from"] == investor
+    assert tx_deposit.events[-1]["amount"] == Web3.toWei(1, "ether")
+    assert tx_deposit.events[-1]["erc20TokenContract"] == erc20_contract
 
 
 def test_deposit_twice(lending_pool_contract, erc20_contract, investor, contract_owner):
     erc20_contract.mint(investor, Web3.toWei(1, "ether"), {"from": contract_owner})
     erc20_contract.approve(lending_pool_contract, Web3.toWei(1, "ether"), {"from": investor})
-    tx_invest = lending_pool_contract.deposit(Web3.toWei(1, "ether"), {"from": investor})
+    tx_deposit = lending_pool_contract.deposit(Web3.toWei(1, "ether"), {"from": investor})
 
     erc20_contract.mint(investor, Web3.toWei(0.5, "ether"), {"from": contract_owner})
     erc20_contract.approve(lending_pool_contract, Web3.toWei(0.5, "ether"), {"from": investor})
-    tx_invest_twice = lending_pool_contract.deposit(Web3.toWei(0.5, "ether"), {"from": investor})
+    tx_deposit_twice = lending_pool_contract.deposit(Web3.toWei(0.5, "ether"), {"from": investor})
 
-    funds_from_address = lending_pool_contract.userFunds(investor)
+    funds_from_address = tx_deposit_twice.return_value
 
-    assert funds_from_address["currentAmountInvested"] == Web3.toWei(1.5, "ether")
-    assert funds_from_address["totalAmountInvested"] == Web3.toWei(1.5, "ether")
+    assert funds_from_address["currentAmountDeposited"] == Web3.toWei(1.5, "ether")
+    assert funds_from_address["totalAmountDeposited"] == Web3.toWei(1.5, "ether")
     assert lending_pool_contract.fundsAvailable() == Web3.toWei(1.5, "ether")
 
 
 def test_withdraw_noinvestment(lending_pool_contract, investor):
-    with brownie.reverts("The sender has no funds invested"):
+    with brownie.reverts("The sender has no funds deposited"):
         lending_pool_contract.withdraw(Web3.toWei(1, "ether"), {"from": investor})
 
 
 def test_withdraw_insufficient_investment(lending_pool_contract, erc20_contract, investor, contract_owner):
     erc20_contract.mint(investor, Web3.toWei(1, "ether"), {"from": contract_owner})
     erc20_contract.approve(lending_pool_contract, Web3.toWei(1, "ether"), {"from": investor})
-    tx_invest = lending_pool_contract.deposit(Web3.toWei(1, "ether"), {"from": investor})
+    tx_deposit = lending_pool_contract.deposit(Web3.toWei(1, "ether"), {"from": investor})
     
-    with brownie.reverts("The sender has less funds invested than the amount requested"):
+    with brownie.reverts("The sender has less funds deposited than the amount requested"):
         lending_pool_contract.withdraw(Web3.toWei(1.5, "ether"), {"from": investor})
 
 
@@ -114,9 +118,7 @@ def test_withdraw(lending_pool_contract, erc20_contract, investor, contract_owne
     erc20_contract.approve(lending_pool_contract, Web3.toWei(2, "ether"), {"from": investor})
     assert user_balance(erc20_contract, investor) == initial_balance + Web3.toWei(2, "ether")
 
-    tx_invest = lending_pool_contract.deposit(Web3.toWei(1, "ether"), {"from": investor})
-    print(lending_pool_contract.userFunds(investor))
-    
+    tx_deposit = lending_pool_contract.deposit(Web3.toWei(1, "ether"), {"from": investor})
     assert user_balance(erc20_contract, investor) == initial_balance + Web3.toWei(1, "ether")
     assert lending_pool_contract.fundsAvailable() == Web3.toWei(1, "ether")
 
@@ -124,18 +126,17 @@ def test_withdraw(lending_pool_contract, erc20_contract, investor, contract_owne
     assert user_balance(erc20_contract, investor) == initial_balance + Web3.toWei(2, "ether")
     assert lending_pool_contract.fundsAvailable() == 0
 
-    funds_from_address = lending_pool_contract.userFunds(investor)
-    print(tx_withdraw.return_value)
-    print(funds_from_address)
-    assert funds_from_address["totalAmountInvested"] == Web3.toWei(1, "ether")
-    assert funds_from_address["currentAmountInvested"] == 0
+    funds_from_address = tx_withdraw.return_value
+    assert funds_from_address["totalAmountDeposited"] == Web3.toWei(1, "ether")
+    assert funds_from_address["currentAmountDeposited"] == 0
     assert funds_from_address["totalAmountWithdrawn"] == Web3.toWei(1, "ether")
+    assert funds_from_address["currentPendingRewards"] == 0
+    assert funds_from_address["activeForRewards"] == False
 
-    assert len(tx_withdraw.events) == 2
+    assert len(tx_withdraw.events) == 3
     assert tx_withdraw.events[-1]["_from"] == investor
     assert tx_withdraw.events[-1]["amount"] == Web3.toWei(1, "ether")
-    assert tx_withdraw.events[-1]["rewards"] == Web3.toWei(0, "ether")
-    assert tx_invest.events[-1]["erc20TokenContract"] == erc20_contract
+    assert tx_deposit.events[-1]["erc20TokenContract"] == erc20_contract
 
 
 def test_send_funds_wrong_sender(lending_pool_contract, investor, borrower):
@@ -158,7 +159,7 @@ def test_send_funds_wrong_amount(lending_pool_contract, erc20_contract, contract
     
     lending_pool_contract.deposit(Web3.toWei(1, "ether"), {"from": investor})
     
-    with brownie.reverts("No sufficient invested funds to perform the transaction"):
+    with brownie.reverts("No sufficient deposited funds to perform the transaction"):
         lending_pool_contract.sendFunds(
             borrower,
             Web3.toWei(2, "ether"),
@@ -223,7 +224,7 @@ def test_receive_funds_zero_value(lending_pool_contract, contract_owner, borrowe
 def test_receive_funds(lending_pool_contract, erc20_contract, contract_owner, investor, borrower):
     erc20_contract.mint(investor, Web3.toWei(1, "ether"), {"from": contract_owner})
     erc20_contract.approve(lending_pool_contract, Web3.toWei(1, "ether"), {"from": investor})
-    tx_invest = lending_pool_contract.deposit(Web3.toWei(1, "ether"), {"from": investor})
+    tx_deposit = lending_pool_contract.deposit(Web3.toWei(1, "ether"), {"from": investor})
     
     tx_send = lending_pool_contract.sendFunds(
         borrower,
@@ -241,15 +242,110 @@ def test_receive_funds(lending_pool_contract, erc20_contract, contract_owner, in
         {"from": contract_owner}
     )
 
-    assert lending_pool_contract.fundsAvailable() == Web3.toWei(1.02, "ether")
+    assert lending_pool_contract.fundsAvailable() == Web3.toWei(1, "ether")
     assert lending_pool_contract.fundsInvested() == 0
     assert lending_pool_contract.totalFundsInvested() == Web3.toWei(0.2, "ether")
     
     assert lending_pool_contract.totalRewards() == Web3.toWei(0.02, "ether")
-    assert lending_pool_contract.rewards() == Web3.toWei(0.02, "ether")
+
+    assert lending_pool_contract.funds(investor)["currentPendingRewards"] == Web3.toWei(0.02, "ether")
+    assert lending_pool_contract.funds(investor)["totalRewardsAmount"] == Web3.toWei(0.02, "ether")
 
     assert len(tx_receive.events) == 3
     assert tx_receive.events[-1]["_from"] == contract_owner
     assert tx_receive.events[-1]["amount"] == Web3.toWei(0.2, "ether")
     assert tx_receive.events[-1]["interestAmount"] == Web3.toWei(0.02, "ether")
     assert tx_send.events[-1]["erc20TokenContract"] == erc20_contract
+
+
+def test_receive_funds_multiple_lenders(lending_pool_contract, erc20_contract, contract_owner, investor, borrower):
+    erc20_contract.mint(investor, Web3.toWei(1, "ether"), {"from": contract_owner})
+    erc20_contract.approve(lending_pool_contract, Web3.toWei(1, "ether"), {"from": investor})
+    lending_pool_contract.deposit(Web3.toWei(1, "ether"), {"from": investor})
+
+    erc20_contract.mint(contract_owner, Web3.toWei(3, "ether"), {"from": contract_owner})
+    erc20_contract.approve(lending_pool_contract, Web3.toWei(3, "ether"), {"from": contract_owner})
+    lending_pool_contract.deposit(Web3.toWei(3, "ether"), {"from": contract_owner})
+
+    tx_send = lending_pool_contract.sendFunds(
+        borrower,
+        Web3.toWei(0.2, "ether"),
+        {"from": contract_owner}
+    )
+
+    erc20_contract.mint(borrower, Web3.toWei(0.22, "ether"), {"from": contract_owner})
+    erc20_contract.approve(lending_pool_contract, Web3.toWei(0.22, "ether"), {"from": borrower})
+
+    tx_receive = lending_pool_contract.receiveFunds(
+        borrower,
+        Web3.toWei(0.2, "ether"),
+        Web3.toWei(0.02, "ether"),
+        {"from": contract_owner}
+    )
+
+    assert lending_pool_contract.fundsAvailable() == Web3.toWei(4, "ether")
+    assert lending_pool_contract.fundsInvested() == 0
+    assert lending_pool_contract.totalFundsInvested() == Web3.toWei(0.2, "ether")
+    
+    assert lending_pool_contract.totalRewards() == Web3.toWei(0.02, "ether")
+
+    expectedLenderOneRewards = Decimal(0.02) * Decimal(1) / Decimal(4)
+    expectedLenderTwoRewards = Decimal(0.02) * Decimal(3) / Decimal(4)
+
+    assert lending_pool_contract.funds(investor)["currentPendingRewards"] == Web3.toWei(expectedLenderOneRewards, "ether")
+    assert lending_pool_contract.funds(investor)["totalRewardsAmount"] == Web3.toWei(expectedLenderOneRewards, "ether")
+    assert lending_pool_contract.funds(contract_owner)["currentPendingRewards"] == Web3.toWei(expectedLenderTwoRewards, "ether")
+    assert lending_pool_contract.funds(contract_owner)["totalRewardsAmount"] == Web3.toWei(expectedLenderTwoRewards, "ether")
+
+    assert len(tx_receive.events) == 3
+    assert tx_receive.events[-1]["_from"] == contract_owner
+    assert tx_receive.events[-1]["amount"] == Web3.toWei(0.2, "ether")
+    assert tx_receive.events[-1]["interestAmount"] == Web3.toWei(0.02, "ether")
+    assert tx_send.events[-1]["erc20TokenContract"] == erc20_contract
+
+
+def test_compound_rewards_no_deposits(lending_pool_contract, investor):
+    with brownie.reverts("The sender has no funds deposited"):
+        lending_pool_contract.compoundRewards({"from": investor})
+
+
+def test_compound_rewards_no_pending_rewards(lending_pool_contract, erc20_contract, contract_owner, investor):
+    erc20_contract.mint(investor, Web3.toWei(1, "ether"), {"from": contract_owner})
+    erc20_contract.approve(lending_pool_contract, Web3.toWei(1, "ether"), {"from": investor})
+    tx_deposit = lending_pool_contract.deposit(Web3.toWei(1, "ether"), {"from": investor})
+    
+    with brownie.reverts("The sender has no pending rewards to compound"):
+        lending_pool_contract.compoundRewards({"from": investor})    
+
+
+def test_compound_rewards(lending_pool_contract, erc20_contract, contract_owner, investor, borrower):
+    erc20_contract.mint(investor, Web3.toWei(1, "ether"), {"from": contract_owner})
+    erc20_contract.approve(lending_pool_contract, Web3.toWei(1, "ether"), {"from": investor})
+    tx_deposit = lending_pool_contract.deposit(Web3.toWei(1, "ether"), {"from": investor})
+    
+    tx_send = lending_pool_contract.sendFunds(
+        borrower,
+        Web3.toWei(0.2, "ether"),
+        {"from": contract_owner}
+    )
+
+    erc20_contract.mint(borrower, Web3.toWei(0.22, "ether"), {"from": contract_owner})
+    erc20_contract.approve(lending_pool_contract, Web3.toWei(0.22, "ether"), {"from": borrower})
+
+    tx_receive = lending_pool_contract.receiveFunds(
+        borrower,
+        Web3.toWei(0.2, "ether"),
+        Web3.toWei(0.02, "ether"),
+        {"from": contract_owner}
+    )
+
+    tx_compound = lending_pool_contract.compoundRewards({"from": investor})
+
+    assert lending_pool_contract.funds(investor)["currentAmountDeposited"] == Web3.toWei(1.02, "ether")
+    assert lending_pool_contract.funds(investor)["currentPendingRewards"] == 0
+    assert lending_pool_contract.funds(investor)["totalRewardsAmount"] == Web3.toWei(0.02, "ether")
+    assert lending_pool_contract.funds(investor)["activeForRewards"] == True
+
+    assert tx_compound.events[-1]["_from"] == investor
+    assert tx_compound.events[-1]["rewards"] == Web3.toWei(0.02, "ether")
+    assert tx_compound.events[-1]["erc20TokenContract"] == erc20_contract
