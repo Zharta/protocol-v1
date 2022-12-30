4484d9730d04692af8d282da784993403b32ce1d
Carlos Ré <carlos.santos.re@gmail.com>
2022-12-30 01:06:32 +0000

    update deployment scripts

---
+++ scripts/helpers/contracts.py
@@ -0,0 +1,266 @@
+from typing import Optional, Any
+from brownie.network.contract import ProjectContract
+from dataclasses import dataclass
+from .types import InternalContract, DeploymentContext
+from .transactions import Transaction
+
+from brownie import (
+    LendingPoolCore,
+    LendingPoolLock,
+    LendingPoolPeripheral,
+    CollateralVaultCore,
+    CollateralVaultPeripheral,
+    LPCMigration01,
+    LoansCore,
+    Loans,
+    LiquidationsCore,
+    LiquidationsPeripheral,
+    LiquidityControls,
+    web3
+)
+
+
+@dataclass
+class CollateralVaultCoreContract(InternalContract):
+
+    def __init__(self, contract: Optional[ProjectContract]):
+        super().__init__(
+            "collateral_vault_core",
+            contract,
+            CollateralVaultCore,
+            container_name="CollateralVaultCore",
+            deployment_deps={},
+            config_deps={
+                "collateral_vault_peripheral": Transaction.cvcore_set_cvperiph,
+            },
+            deployment_args_contracts=[],
+        )
+
+
+@dataclass
+class CollateralVaultPeripheralContract(InternalContract):
+
+    def __init__(self, contract: Optional[ProjectContract]):
+        super().__init__(
+            "collateral_vault_peripheral",
+            contract,
+            CollateralVaultPeripheral,
+            container_name="CollateralVaultPeripheral",
+            deployment_deps={"collateral_vault_core"},
+            config_deps={
+                "liquidations_peripheral": Transaction.cvperiph_set_liquidationsperiph,
+                "loans": Transaction.cvperiph_add_loansperiph,
+            },
+            deployment_args_contracts=["collateral_vault_core"],
+        )
+
+
+@dataclass
+class LendingPoolCoreContract(InternalContract):
+
+    def __init__(self, contract: Optional[ProjectContract]):
+        super().__init__(
+            "lending_pool_core",
+            contract,
+            LendingPoolCore,
+            container_name="LendingPoolCore",
+            deployment_deps=["weth"],
+            config_deps={"lending_pool_peripheral": Transaction.lpcore_set_lpperiph},
+            deployment_args_contracts=["weth"],
+        )
+
+
+@dataclass
+class LendingPoolLockContract(InternalContract):
+
+    def __init__(self, contract: Optional[ProjectContract]):
+        super().__init__(
+            "lending_pool_lock",
+            contract,
+            LendingPoolLock,
+            container_name="LendingPoolLock",
+            deployment_deps=["weth"],
+            config_deps={
+                "lending_pool_peripheral": Transaction.lpcore_set_lpperiph,
+                "lenders_with_active_locks": Transaction.lplock_migrate
+            },
+            deployment_args_contracts=["weth"],
+        )
+
+
+@dataclass
+class LPCMigration01Contract(InternalContract):
+
+    def __init__(self, contract: Optional[ProjectContract]):
+        super().__init__(
+            "lpc_migration_01",
+            contract,
+            LPCMigration01,
+            container_name="LPCMigration01",
+            deployment_deps=["lending_pool_core", "legacy_lending_pool_core"],
+            config_deps={"run_lpc_migration_01": Transaction.lpc_migration_migrate},
+            deployment_args_contracts=["legacy_lending_pool_core", "lending_pool_core"],
+        )
+
+
+@dataclass
+class LendingPoolPeripheralContract(InternalContract):
+
+    def __init__(self, contract: Optional[ProjectContract]):
+        super().__init__(
+            "lending_pool_peripheral",
+            contract,
+            LendingPoolPeripheral,
+            container_name="LendingPoolPeripheral",
+            deployment_deps={"lending_pool_core", "lending_pool_lock", "weth"},
+            config_deps={
+                "loans": Transaction.lpperiph_set_loansperiph,
+                "liquidations_peripheral": Transaction.lpperiph_set_liquidationsperiph,
+                "liquidity_controls": Transaction.lpperiph_set_liquiditycontrols,
+            },
+        )
+
+    def deployment_args(self, context: DeploymentContext) -> list[Any]:
+        return [
+            context["lending_pool_core"].contract,
+            context["lending_pool_lock"].contract,
+            context["weth"].contract,
+            context.owner,
+            2500,
+            7000,
+            False,
+        ]
+
+    def config_key(self):
+        return "lending_pool"
+
+
+@dataclass
+class LoansCoreContract(InternalContract):
+
+    def __init__(self, contract: Optional[ProjectContract]):
+        super().__init__(
+            "loans_core",
+            contract,
+            LoansCore,
+            container_name="LoansCore",
+            deployment_deps={},
+            config_deps={"loans": Transaction.loanscore_set_loansperiph},
+            deployment_args_contracts=[],
+        )
+
+
+@dataclass
+class LoansPeripheralContract(InternalContract):
+
+    def __init__(self, contract: Optional[ProjectContract]):
+        super().__init__(
+            "loans",
+            contract,
+            Loans,
+            container_name="Loans",
+            deployment_deps={"loans_core", "lending_pool_peripheral", "collateral_vault_peripheral"},
+            config_deps={
+                "liquidations_peripheral": Transaction.loansperiph_set_liquidationsperiph,
+                "liquidity_controls": Transaction.loansperiph_set_liquiditycontrols,
+                "lending_pool_peripheral": Transaction.loansperiph_set_lpperiph,
+                "collateral_vault_peripheral": Transaction.loansperiph_set_cvperiph,
+                "cool_cats": Transaction.loansperiph_add_collateral_cool_cats,
+                "hashmasks": Transaction.loansperiph_add_collateral_hashmasks,
+                "bakc": Transaction.loansperiph_add_collateral_bakc,
+                "doodles": Transaction.loansperiph_add_collateral_doodles,
+                "wow": Transaction.loansperiph_add_collateral_wow,
+                "mayc": Transaction.loansperiph_add_collateral_mayc,
+                "veefriends": Transaction.loansperiph_add_collateral_veefriends,
+                "pudgy_penguins": Transaction.loansperiph_add_collateral_pudgy_penguins,
+                "bayc": Transaction.loansperiph_add_collateral_bayc,
+                "wpunks": Transaction.loansperiph_add_collateral_wpunks,
+                "cryptopunks": Transaction.loansperiph_add_collateral_punks,
+            },
+        )
+
+    def deployment_args(self, context: DeploymentContext) -> list[Any]:
+        return [
+            1000000,
+            31 * 86400,
+            web3.toWei(10000, "ether"),
+            24 * 60 * 60,
+            context["loans_core"].contract,
+            context["lending_pool_peripheral"].contract,
+            context["collateral_vault_peripheral"].contract,
+        ]
+
+
+@dataclass
+class LiquidationsCoreContract(InternalContract):
+
+    def __init__(self, contract: Optional[ProjectContract]):
+        super().__init__(
+            "liquidations_core",
+            contract,
+            LiquidationsCore,
+            container_name="LiquidationsCore",
+            deployment_deps={},
+            config_deps={
+                "liquidations_core": Transaction.liquidationscore_set_liquidationsperiph,
+                "loans_core": Transaction.liquidationscore_add_loanscore,
+
+            },
+            deployment_args_contracts=[],
+        )
+
+
+@dataclass
+class LiquidationsPeripheralContract(InternalContract):
+
+    def __init__(self, contract: Optional[ProjectContract]):
+        super().__init__(
+            "liquidations_peripheral",
+            contract,
+            LiquidationsPeripheral,
+            container_name="LiquidationsPeripheral",
+            deployment_deps={"liquidations_core", "weth", "collateral_vault_peripheral"},
+            config_deps={
+                "liquidations_core": Transaction.liquidationsperiph_set_liquidationscore,
+                "loans_core": Transaction.liquidationsperiph_add_loanscore,
+                "lending_pool_peripheral": Transaction.liquidationsperiph_add_lpperiph,
+                "collateral_vault_peripheral": Transaction.liquidationsperiph_set_cvperiph,
+                "nftxvaultfactory": Transaction.liquidationsperiph_set_nftxvaultfactory,
+                "nftxmarketplacezap": Transaction.liquidationsperiph_set_nftxmarketplacezap,
+                "sushirouter, ": Transaction.liquidationsperiph_set_sushirouter,
+            },
+        )
+
+    def deployment_args(self, context: DeploymentContext) -> list[Any]:
+        return [
+            context["liquidations_core"].contract,
+            2 * 86400,
+            2 * 86400,
+            2 * 86400,
+            context["weth"].contract,
+        ]
+
+
+@dataclass
+class LiquidityControlsContract(InternalContract):
+
+    def __init__(self, contract: Optional[ProjectContract]):
+        super().__init__(
+            "liquidity_controls",
+            contract,
+            LiquidityControls,
+            container_name="LiquidityControls",
+            deployment_deps={},
+            config_deps={"nft_borrowable_amounts": Transaction.liquiditycontrols_change_collectionborrowableamounts},
+        )
+
+    def deployment_args(self, context: DeploymentContext) -> list[Any]:
+        return [
+            False,
+            1500,
+            False,
+            7 * 24 * 60 * 60,
+            False,
+            1500,
+            False,
+        ]
---
+++ scripts/helpers/transactions.py
@@ -0,0 +1,183 @@
+from dataclasses import dataclass
+from typing import Callable
+from .types import DeploymentContext
+
+
+@dataclass
+class Transaction:
+
+    @staticmethod
+    def lpcore_set_lpperiph(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "lending_pool_core", "setLendingPoolPeripheralAddress", "lending_pool_peripheral", dryrun=dryrun)
+
+    @staticmethod
+    def lplock_set_lpperiph(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "lending_pool_lock", "setLendingPoolPeripheralAddress", "lending_pool_peripheral", dryrun=dryrun)
+
+    @staticmethod
+    def lpc_migration_migrate(context: DeploymentContext, dryrun: bool = False):
+        if not context["run_lpc_migration_01"]:
+            print("Skipping lpc_migration_01")
+            return
+        execute(context, "lending_pool_core", "proposeOwner", "lpc_migration_01", dryrun=dryrun)
+        execute(context, "legacy_lending_pool_core", "proposeOwner", "lpc_migration_01", dryrun=dryrun)
+        execute(context, "lpc_migration_01", "migrate", dryrun=dryrun)
+        execute(context, "lending_pool_core", "claimOwnership", dryrun=dryrun)
+
+    @staticmethod
+    def lplock_migrate(context: DeploymentContext, dryrun: bool = False):
+        lending_pool_lock = context["lending_pool_lock"].contract
+        legacy_lending_pool_core = context["legacy_lending_pool_core"].contract
+        lenders_with_active_locks = context["lenders_with_active_locks"]
+        print(f"## lending_pool_lock.migrate(legacy_lending_pool_core, {lenders_with_active_locks})")
+        if not dryrun:
+            lending_pool_lock.migrate(legacy_lending_pool_core, lenders_with_active_locks, {"from": context.owner})
+
+    @staticmethod
+    def lpperiph_set_loansperiph(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "lending_pool_peripheral", "setLoansPeripheralAddress", "loans", dryrun=dryrun)
+
+    @staticmethod
+    def lpperiph_set_liquidationsperiph(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "lending_pool_peripheral", "setLiquidationsPeripheralAddress", "liquidations_peripheral", dryrun=dryrun)
+
+    @staticmethod
+    def lpperiph_set_liquiditycontrols(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "lending_pool_peripheral", "setLiquidityControlsAddress", "liquidity_controls", dryrun=dryrun)
+
+    @staticmethod
+    def cvcore_set_cvperiph(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "collateral_vault_core", "setCollateralVaultPeripheralAddress", "collateral_vault_peripheral", dryrun=dryrun)
+
+    @staticmethod
+    def cvperiph_add_loansperiph(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "collateral_vault_peripheral", "addLoansPeripheralAddress", "weth", "loans", dryrun=dryrun)
+
+    @staticmethod
+    def cvperiph_set_liquidationsperiph(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "collateral_vault_peripheral", "setLiquidationsPeripheralAddress", "liquidations_peripheral", dryrun=dryrun)
+
+    @staticmethod
+    def loanscore_set_loansperiph(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "loans_core", "setLoansPeripheral", "loans", dryrun=dryrun)
+
+    @staticmethod
+    def loansperiph_set_liquidationsperiph(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "loans", "setLiquidationsPeripheralAddress", "liquidations_peripheral", dryrun=dryrun)
+
+    @staticmethod
+    def loansperiph_set_liquiditycontrols(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "loans", "setLiquidityControlsAddress", "liquidity_controls", dryrun=dryrun)
+
+    @staticmethod
+    def loansperiph_set_lpperiph(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "loans", "setLendingPoolPeripheralAddress", "lending_pool_peripheral", dryrun=dryrun)
+
+    @staticmethod
+    def loansperiph_set_cvperiph(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "loans", "setCollateralVaultPeripheralAddress", "collateral_vault_peripheral", dryrun=dryrun)
+
+    @staticmethod
+    def liquidationscore_set_liquidationsperiph(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "liquidations_core", "setLiquidationsPeripheralAddress", "liquidations_peripheral", dryrun=dryrun)
+
+    @staticmethod
+    def liquidationscore_add_loanscore(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "liquidations_core", "addLoansCoreAddress", "weth", "loans_core", dryrun=dryrun)
+
+    @staticmethod
+    def liquidationsperiph_set_liquidationscore(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "liquidations_peripheral", "setLiquidationsCoreAddress", "liquidations_core", dryrun=dryrun)
+
+    @staticmethod
+    def liquidationsperiph_add_loanscore(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "liquidations_peripheral", "addLoansCoreAddress", "weth", "loans_core", dryrun=dryrun)
+
+    @staticmethod
+    def liquidationsperiph_add_lpperiph(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "liquidations_peripheral", "addLendingPoolPeripheralAddress", "weth", "lending_pool_peripheral", dryrun=dryrun)
+
+    @staticmethod
+    def liquidationsperiph_set_cvperiph(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "liquidations_peripheral", "setCollateralVaultPeripheralAddress", "collateral_vault_peripheral", dryrun=dryrun)
+
+
+    @staticmethod
+    def liquidationsperiph_set_nftxvaultfactory(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "liquidations_peripheral", "setNFTXVaultFactoryAddress", "nftxvaultfactory", dryrun=dryrun)
+
+    @staticmethod
+    def liquidationsperiph_set_nftxmarketplacezap(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "liquidations_peripheral", "setNFTXMarketplaceZapAddress", "nftxmarketplacezap", dryrun=dryrun)
+
+    @staticmethod
+    def liquidationsperiph_set_sushirouter(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "liquidations_peripheral", "setSushiRouterAddress", "sushirouter", dryrun=dryrun)
+
+    @staticmethod
+    def loansperiph_add_collateral_cool_cats(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "loans", "addCollateralToWhitelist", "cool_cats", dryrun=dryrun)
+
+    @staticmethod
+    def loansperiph_add_collateral_hashmasks(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "loans", "addCollateralToWhitelist", "hashmasks", dryrun=dryrun)
+
+    @staticmethod
+    def loansperiph_add_collateral_bakc(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "loans", "addCollateralToWhitelist", "bakc", dryrun=dryrun)
+
+    @staticmethod
+    def loansperiph_add_collateral_doodles(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "loans", "addCollateralToWhitelist", "doodles", dryrun=dryrun)
+
+    @staticmethod
+    def loansperiph_add_collateral_wow(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "loans", "addCollateralToWhitelist", "wow", dryrun=dryrun)
+
+    @staticmethod
+    def loansperiph_add_collateral_mayc(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "loans", "addCollateralToWhitelist", "mayc", dryrun=dryrun)
+
+    @staticmethod
+    def loansperiph_add_collateral_veefriends(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "loans", "addCollateralToWhitelist", "veefriends", dryrun=dryrun)
+
+    @staticmethod
+    def loansperiph_add_collateral_pudgy_penguins(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "loans", "addCollateralToWhitelist", "pudgy_penguins", dryrun=dryrun)
+
+    @staticmethod
+    def loansperiph_add_collateral_bayc(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "loans", "addCollateralToWhitelist", "bayc", dryrun=dryrun)
+
+    @staticmethod
+    def loansperiph_add_collateral_wpunks(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "loans", "addCollateralToWhitelist", "wpunks", dryrun=dryrun)
+
+    @staticmethod
+    def loansperiph_add_collateral_punks(context: DeploymentContext, dryrun: bool = False):
+        execute(context, "loans", "addCollateralToWhitelist", "cryptopunks", dryrun=dryrun)
+
+    @staticmethod
+    def liquiditycontrols_change_collectionborrowableamounts(context: DeploymentContext, dryrun: bool = False):
+        nft_borrowable_amounts = context["nft_borrowable_amounts"]
+        contract_instance = context["liquidity_controls"].contract
+        for nft, value_wei in nft_borrowable_amounts.items():
+            address = context[nft].address()
+            args = [True, address, value_wei, {"from": context.owner}]
+            if dryrun:
+                print(f"## liquidity_controls.changeMaxCollectionBorrowableAmount({','.join(str(a) for a in args)}")
+            elif contract_instance.maxCollectionBorrowableAmount(address) != value_wei:
+                print(f"## liquidity_controls.changeMaxCollectionBorrowableAmount({','.join(str(a) for a in args)}")
+                contract_instance.changeMaxCollectionBorrowableAmount(*args)
+            else:
+                print(f"Skip changeMaxCollectionBorrowableAmount for {nft}, current addres is already {address}")
+
+
+def execute(context: DeploymentContext, contract: str, func: str, *args, dryrun: bool = False):
+    contract_instance = context.contract[contract].contract
+    print(f"## {contract}.{func}({','.join(args)})")
+    if not dryrun:
+        function = getattr(contract_instance, func)
+        deploy_args = [context[a].address() for a in args]
+        function(*deploy_args, {"from": context.owner})
