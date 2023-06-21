f48752acb262017c218cb27ff68a715f44b8d62d
Carlos Ré <carlos.re@zharta.com>
2023-06-21 13:39:34 +0100

    deploy to dev

--- ape_console_extras.py
+++ ape_console_extras.py
@@ -14,6 +14,16 @@ def transfer(w3, wallet, val=10**60):
     w3.eth.send_transaction({"from": b, "to": wallet, "value": val})
     print(f"new balance: {w3.eth.get_balance(wallet)}")
 
+def propose_owner(dm, from_wallet, to_wallet):
+    contracts = [c for c in dm.context.contract.values() if hasattr(c.contract, "proposeOwner")]
+    for c in contracts:
+        c.contract.proposeOwner(to_wallet, sender=from_wallet)
+
+def claim_ownership(dm, wallet):
+    contracts = [c for c in dm.context.contract.values() if hasattr(c.contract, "claimOwnership")]
+    for c in contracts:
+        c.contract.claimOwnership(sender=wallet)
+
 def ape_init_extras(network):
     dm = DeploymentManager(ENV)
 
